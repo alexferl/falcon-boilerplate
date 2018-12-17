6b37f48b0900810f3bfb95bdd526b03e72d9eb84
admiralobvious <aferlandqc@gmail.com>
2018-12-17 15:29:02 -0500

    update vyper and fix vyper order

--- app/util/config.py
+++ app/util/config.py
@@ -9,6 +9,8 @@ def setup_vyper(parser):
     env_name = os.getenv("APP_ENV_NAME", "LOCAL").lower()
     config_name = "config.{}".format(env_name)
 
+    v.bind_args(parser)
+
     v.set_env_prefix(v.get("environment_variables_prefix"))
     v.set_env_key_replacer("-", "_")
     v.automatic_env()
@@ -17,5 +19,3 @@ def setup_vyper(parser):
     v.set_config_type("toml")
     v.set_config_name(config_name)
     v.read_in_config()
-
-    v.bind_args(parser)
