38bfbc6e9cf43ff6efef605fe181fe90ad821f83
admiralobvious <aferlandqc@gmail.com>
2020-05-10 14:24:53 -0400

    rapidjson is required

--- app/media/json.py
+++ app/media/json.py
@@ -1,20 +1,17 @@
-try:
-    import rapidjson as json
-except ImportError:  # pragma: no cover
-    import json
+import rapidjson
 
 
 def dump(obj: object, stream: bytes, *args, **kwargs) -> bytes:
-    return json.dump(obj, stream, *args, **kwargs)
+    return rapidjson.dump(obj, stream, *args, **kwargs)
 
 
 def dumps(obj: object, *args, **kwargs) -> bytes:
-    return json.dumps(obj, *args, **kwargs)
+    return rapidjson.dumps(obj, *args, **kwargs)
 
 
 def load(stream: bytes, *args, **kwargs) -> dict:
-    return json.load(stream, *args, **kwargs)
+    return rapidjson.load(stream, *args, **kwargs)
 
 
 def loads(string: str, *args, **kwargs) -> dict:
-    return json.loads(string, *args, **kwargs)
+    return rapidjson.loads(string, *args, **kwargs)
